422d9fdc7ce920568a6f9e425b47635dde48dbe7
Tsvetan Igov <ts.igov@outlook.com>
2025-01-02 02:45:33 +0200

    Reworked scripts to use git-crypt instead

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p git age git-crypt
+
+set -e
+
+REPO_LOCATION=$HOME/repositories/tsIgov/hosts-config
+
+BRANCH_NAME=$1
+if [ -z $BRANCH_NAME ]; then
+	BRANCH_NAME=$(hostname)
+fi
+
+SCRIPT_PATH=$(realpath $0)
+
+clone_repo
+unlock_repo
+link_configs
+install
+
+rm $SCRIPT_PATH
+
+clone_repo() {
+	git clone --single-branch --branch $BRANCH_NAME https://github.com/tsIgov/hosts-config.git $REPO_LOCATION
+}
+
+unlock_repo() {
+	cd $REPO_LOCATION
+
+	while ! age -d -o $BRANCH_NAME key.age
+	do
+		echo "Try again"
+	done
+
+	git-crypt unlock $BRANCH_NAME
+	rm -f $BRANCH_NAME
+}
+
+link_configs() {
+	sudo rm -f /etc/nixos
+	sudo ln -s $REPO_LOCATION/src /etc/nixos
+}
+
+install() {
+	sudo nixos-generate-config
+	sudo nixos-rebuild switch --flake $(realpath /etc/nixos)#$BRANCH_NAME
+}
